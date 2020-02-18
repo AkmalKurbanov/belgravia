997d66f723752c023cb1dd9d0ef7bcf09d00fea3
AkmalKurbanov <250296a@gmail.com>
2020-02-19 01:43:48 +0600

    first commit

---
+++ src/js/scripts/slider.js
@@ -0,0 +1,13 @@
+     var swiper = new Swiper('.entry-js', {
+       navigation: {
+         nextEl: '.next',
+         prevEl: '.prev',
+       }
+     });
+     var swiper = new Swiper('.gallery-js', {
+       simulateTouch: false,
+       pagination: {
+         el: '.swiper-pagination',
+         clickable: true,
+       },
+     });
